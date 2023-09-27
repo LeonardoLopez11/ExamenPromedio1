e22be4e64f0baf96db821072c2ee7449555fdcab
LeonardoLopez11 <85042599+LeonardoLopez11@users.noreply.github.com>
2023-09-26 20:55:00 -0500

    metodo y listas de granja

--- ExamenPromedio1/Granja.cs
+++ ExamenPromedio1/Granja.cs
@@ -9,6 +9,17 @@ namespace ExamenPromedio1
     class Granja
     {
 
+        private int PrecioExpansion;
+        List<Animal> animales = new List<Animal>();
+        Animal Cerdo = new Animal("CERDO", 500, 50, 10, 50);
+        Animal Vaca = new Animal("VACA", 500, 100, 15, 10);
+
+        public void ExpandirGranja()
+        {
+            PrecioExpansion = PrecioExpansion+ 10;
+            Console.WriteLine("Se ha completado la compra");
+        }
+
 
 
     }
